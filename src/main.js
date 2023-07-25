4a217846c597730c69a0df41fb5804679194f3f7
wahyu.esya17 <wahyu.esya17@gmail.com>
2023-07-25 10:04:00 +0700

    change version

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 
-Vue.config.productionTip = false;
+const { version } = "../package.json";
 
-const version = 2;
+Vue.config.productionTip = false;
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
